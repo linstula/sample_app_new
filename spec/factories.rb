b0c670bd2030b514064e0b2c5d146f2bee22e7f0
Lin Reid <linreid@gmail.com>
2013-02-01 15:21:31 -0500

    Finish sign in

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
 	factory :user do
-		name										"Michael Hartl"
-		email										"michael@example.com"
-		password								"foobar"
+		name						"Michael Hartl"
+		email						"michael@example.com"
+		password					"foobar"
 		password_confirmation		"foobar"
 	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  password_digest :string(255)
+#  remember_token  :string(255)
 #
 
 require 'spec_helper'
@@ -26,6 +27,7 @@ describe User do
 	it { should respond_to(:password_digest) }
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:remember_token) }
 	it { should respond_to(:authenticate) }
 
 	it { should be_valid }
@@ -122,4 +124,8 @@ describe User do
 		end
 	end
 
+	describe "remember token" do
+		before { @user.save }
+		its(:remember_token) { should_not be_blank }
+	end
 end
